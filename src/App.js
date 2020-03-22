263936f5f8e2bbe1b7710ca9d3349e2e24d5efd8
Vasco Silva <vasco.s.rebolo@gmail.com>
2020-03-22 22:06:04 +0000

    Add initial logic (all in one function, to refactor later)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import Card from './components/card'
 
 function App() {
+  const [ players, setPlayers ] = useState(0)
+  const [ step, setStep ] = useState(0)
+  const [ names, setNames ] = useState({})
+  const [ coins, setCoins ] = useState({})
+  const [ round, setRound ] = useState(1)
+
+  const handleSetPlayers = (nr) => {
+    setStep(1)
+    setPlayers(nr)
+  }
+
+  const handleNameChange = (e) => {
+    const { value, dataset } = e.target
+    const entry = {}
+
+    entry[dataset.id] = value
+    setNames({ ...names, ...entry})
+  }
+
+  const renderNameInputs = () => {
+    let inputs = []
+
+    for (let i = 0; i < players; i++) {
+      inputs.push(
+        <>
+          <label key="name-{i}">
+            Name {i + 1}
+            <input
+              data-id={`name-${i}`}
+              type="text"
+              onChange={handleNameChange}
+            />
+          </label>
+        </>
+      )
+    }
+
+    return inputs
+  }
+
+  const namesSubmitHanlder = (e) => {
+    e.preventDefault()
+    setStep(2)
+  }
+
+  const renderNamesList = () => {
+    const namesItems = []
+
+    for (const name in names) {
+      namesItems.push(
+      <p>{ names[name] }</p>
+      )
+    }
+
+    return namesItems
+  }
+
+  const renderStep = () => {
+    switch (step) {
+      case 1:
+        return (
+          <Card>
+            <p>What are the names of the { players } players</p>
+            <form onSubmit={namesSubmitHanlder}>
+              { renderNameInputs() }
+              <input type="submit" />
+            </form>
+          </Card>
+        )
+      case 2:
+          return (
+            <Card>
+              <h2>Names</h2>
+              { renderNamesList() }
+            </Card>
+          )
+      default:
+        return (
+          <Card>
+            <p>How many players ?</p>
+            <div>
+              <button onClick={() => handleSetPlayers(2)}>2</button>
+              <button onClick={() => handleSetPlayers(3)}>3</button>
+              <button onClick={() => handleSetPlayers(4)}>4</button>
+              <button onClick={() => handleSetPlayers(5)}>5</button>
+            </div>
+          </Card>
+        )
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { renderStep() }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/card.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const CardStyled = styled.div`
+  align-items: center;
+  background: red;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  width: 100vw;
+`
+
+const Card = ({ children }) => {
+  return (
+    <CardStyled>
+      { children }
+    </CardStyled>
+  )
+}
+
+Card.displayName = 'Card'
+
+export default Card
